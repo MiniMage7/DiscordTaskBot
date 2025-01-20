63d99768b749b571d55e2ee2a7f5fad8ee1d64d3
Winter <35548172+MiniMage7@users.noreply.github.com>
2025-01-19 21:56:57 -0600

    Added first bot command
    Added new necessary intent and import
    Added a check for if a user is an admin
    Added a command that makes a task list for a user. Currently bare-bones

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import discord
+from discord.ext import commands
 import os
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-client = MyClient(intents=intents)
-client.run(os.environ['TOKEN'])
+
+@bot.check
+async def is_admin(ctx):
+    return ctx.author.guild_permissions.administrator or await bot.is_owner(ctx.author)
+
+
+@bot.command()
+@commands.check(is_admin)
+async def create_new_task_list(ctx, *args):
+    # Delete the message that called this function
+    await ctx.message.delete()
+
+    if len(args) != 1:
+        await ctx.send("create_new_task_list requires only a user_id\nExample: $create_new_task_list 1234567890")
+        return
+
+    # Get the user from the user_id
+    user = bot.get_user(int(args[0]))
+    if user is None:
+        await ctx.send("Invalid user_id")
+        return
+
+    # Create the embedded message
+    embed_var = discord.Embed(title=user.display_name + "'s Tasks")
+
+    # Send the embedded message
+    embedded_message = await ctx.channel.send(embed=embed_var)
+
+
+bot.run(os.environ['TOKEN'])
